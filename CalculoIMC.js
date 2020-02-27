b1d68fb6f817229a931fcd4752280b60da3944d0
Walter Silva <57921555+walterowisk@users.noreply.github.com>
2020-02-27 15:45:15 -0300

    Update CalculoIMC.js

--- CalculoIMC.js
+++ CalculoIMC.js
@@ -1,4 +1,4 @@
-/Cálculo de IMC
+//Cálculo de IMC
 //Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
 //Comece criando constantes para armazenar o nome, peso, altura e sexo de uma pessoa.
 
